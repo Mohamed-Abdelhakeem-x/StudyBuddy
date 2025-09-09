1d5b12483e543fea8c28e77de15a26a1f7f9da30
Mohamed Abdelhakeem <mohamed.abdelhakeemx@gmail.com>
2025-09-09 22:11:44 +0300

    Created templates and edited urls, views and settings

--- base/views.py
+++ base/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+rooms = [
+    {'id':1, 'name':'lets learn Python!'},
+    {'id':2, 'name':'lets learn C++!'},
+    {'id':3, 'name':'lets learn Go!'},
+]
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    return render(request, 'base/room.html')
