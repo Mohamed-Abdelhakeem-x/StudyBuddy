6d673514f1985a2c572c6323ba17f81b329092ec
Mohamed Abdelhakeem <mohamed.abdelhakeemx@gmail.com>
2025-09-10 08:11:29 +0300

    Created Models and done migrations!

--- base/views.py
+++ base/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
-rooms = [
+# Create your views here
+
+""" rooms = [
     {'id':1, 'name':'lets learn Python!'},
     {'id':2, 'name':'lets learn C++!'},
     {'id':3, 'name':'lets learn Go!'},
-]
+]  """
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    return render(request, 'base/room.html')
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
