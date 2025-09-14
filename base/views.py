8c0cf383029f8b9656f67de34b8ae75c937b65c0
Mohamed Abdelhakeem <mohamed.abdelhakeemx@gmail.com>
2025-09-14 04:05:14 +0300

    room full functioning chatting feature added successfully

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Room, Topic
-from .forms import RoomForm
+from .models import Room, Topic, Message
+from .forms import RoomForm, MessageForm
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib import messages
@@ -71,7 +71,19 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+    
+    context = {'room': room, 'room_messages':room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='Login')
@@ -113,3 +125,30 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+@login_required(login_url='Login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!!')
+    if request.method == 'POST':
+        message.delete()
+
+    return render(request, 'base/delete.html', {'obj': message})
+
+@login_required(login_url='Login')
+def updateMessage(request, pk):
+    message = Message.objects.get(id=pk)
+    form = MessageForm(instance=message)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!!')
+    
+    if request.method == 'POST':
+        form = MessageForm(request.POST, instance=message)
+        if form.is_valid():
+            form.save()
+            return redirect('room', pk=message.room.id)
+    
+    context = {'form': form}
+    return render(request, 'base/message_form.html', context)
