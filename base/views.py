dffd26c296328f638c563a8f20fd1074b9600bfe
Mohamed Abdelhakeem <mohamed.abdelhakeemx@gmail.com>
2025-09-18 04:51:39 +0300

    mobile responsivness done!

--- base/views.py
+++ base/views.py
@@ -65,7 +65,7 @@ def home(request):
         Q(host__username__icontains = q)
         )
      
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains = q))
 
@@ -189,3 +189,14 @@ def updateUser(request):
 
     context = {'form': form}
     return render(request, 'base/update-user.html', context)
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    context = {'topics': topics}
+    return render(request, 'base/topics.html', context)
+
+def activityPage(request):
+    room_messages = Message.objects.all().order_by('-created')
+    context = {'room_messages': room_messages}
+    return render(request, 'base/activity.html', context)
